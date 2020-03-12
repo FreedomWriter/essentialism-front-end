2dd9aa6cae5ed4aca8bcc1cd007ac3f3e068e9f7
FreedomWriter <natalienetworks@gmail.com>
2020-03-12 00:12:37 -0500

    backend debugged for post user-values, will now do same for front end

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,11 +4,11 @@ export const axiosWithAuth = () => {
   const token = JSON.parse(localStorage.getItem("token"));
 
   return axios.create({
-    baseURL: "http://localhost:5000/api",
-    // "https://essentialism-back-end.herokuapp.com/api",
+    baseURL: "https://essentialism-back-end.herokuapp.com/api",
     headers: {
       Authorization: token
     }
   });
 };
 // https://essentialism-back-end.herokuapp.com/api
+// "http://localhost:5000/api",
