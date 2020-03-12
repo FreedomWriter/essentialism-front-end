166542f832e0f8412c82c7da07f4dd3c02ceae7a
FreedomWriter <natalienetworks@gmail.com>
2020-03-12 14:35:51 -0500

    postUserValues behaving as expected

--- src/store/actions/user-values.actions.js
+++ src/store/actions/user-values.actions.js
@@ -41,8 +41,15 @@ export const getUserValues = userId => dispatch => {
 
 export const postUserValues = (id, values) => dispatch => {
   dispatch({ type: USER_VALUES_POST_START, payload: values });
+  console.log(`user-values.actions.js: postUserValues: values: `, {
+    user_value: values.value,
+    user_value_description: values.value_description
+  });
   return axiosWithAuth()
-    .post(`/user/${id}/values`, values)
+    .post(`/user/${id}/values`, {
+      user_value: values.value,
+      user_value_description: values.value_description
+    })
     .then(res => {
       dispatch({
         type: USER_VALUES_POST_SUCCESS,
