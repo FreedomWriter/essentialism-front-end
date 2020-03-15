3173ca172cacf1413e69b1fdeb5700f31ae6bd98
Natalie Davis <nataliedavis@Natalies-MacBook-Pro.local>
2020-03-14 20:02:40 -0500

    projects rendering on homepage

--- src/components/pages/homepage/HomePage.styles.jsx
+++ src/components/pages/homepage/HomePage.styles.jsx
@@ -1,24 +1,24 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-import ConfirmedValues from "../../components/confirmed-values/Confirmed-Values.component";
-import ProjectList from "../../components/project-list/ProjectList.component";
+// import ConfirmedValues from "../../components/confirmed-values/Confirmed-Values.component";
+// import ProjectList from "../../components/project-list/ProjectList.component";
 
-export const StyledSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: flex-start;
-  margin-bottom: 5vh;
-`;
+// export const StyledSection = styled.section`
+//   display: flex;
+//   flex-flow: column nowrap;
+//   align-items: flex-start;
+//   margin-bottom: 5vh;
+// `;
 
-export const StyledConfirmedValues = styled(ConfirmedValues)`
-  max-width: 30vw;
-  width: 200px;
-  margin-top: 5%;
-  margin-bottom: 5%;
-`;
+// export const StyledConfirmedValues = styled(ConfirmedValues)`
+//   max-width: 30vw;
+//   width: 200px;
+//   margin-top: 5%;
+//   margin-bottom: 5%;
+// `;
 
-export const StyledProjectList = styled(ProjectList)`
-  max-width: 30vw;
-  width: 200px;
-  margin-top: 0;
-`;
+// export const StyledProjectList = styled(ProjectList)`
+//   max-width: 30vw;
+//   width: 200px;
+//   margin-top: 0;
+// `;
