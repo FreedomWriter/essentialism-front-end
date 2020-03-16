25b8822736995f652568bb16c049cd179a8adbca
Natalie Davis <nataliedavis@Natalies-MacBook-Pro.local>
2020-03-16 13:50:09 -0500

    removed logger

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const persistor = persistStore(store);
 
