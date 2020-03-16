232ff31520392605edebf343eab681d7acc55eb2
Natalie Davis <nataliedavis@Natalies-MacBook-Pro.local>
2020-03-16 13:32:10 -0500

    updated

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = JSON.parse(localStorage.getItem("token"));
 
   return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://essentialism-back-end.herokuapp.com/api",
     headers: {
       Authorization: token
     }
