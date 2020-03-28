cd6e3dbefe7ee354ffc22c046cb1c9d6ea5dae47
FreedomWriter <natalienetworks@gmail.com>
2020-03-27 22:11:31 -0500

    working on adding tasks

--- src/store/reducers/projectsReducer.js
+++ src/store/reducers/projectsReducer.js
@@ -13,8 +13,30 @@ import {
   PROJECTS_DELETE_FAILURE
 } from "../actions/projects.actions";
 
+import {
+  TASKS_LOAD_START,
+  TASKS_LOAD_SUCCESS,
+  TASKS_LOAD_FAILURE,
+  TASKS_POST_START,
+  TASKS_POST_SUCCESS,
+  TASKS_POST_FAILURE,
+  TASKS_PUT_START,
+  TASKS_PUT_SUCCESS,
+  TASKS_PUT_FAILURE,
+  TASKS_DELETE_START,
+  TASKS_DELETE_SUCCESS,
+  TASKS_DELETE_FAILURE
+} from "../actions/tasks.actions";
+
 const initialState = {
-  projects: []
+  projects: [
+    {
+      project: {},
+      tasks: [],
+      resources: [],
+      contexts: []
+    }
+  ]
 };
 const projectsReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -90,6 +112,83 @@ const projectsReducer = (state = initialState, action) => {
         projects: action.payload
       };
     case PROJECTS_DELETE_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case TASKS_LOAD_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case TASKS_LOAD_SUCCESS:
+      return {
+        ...state,
+        tasks: action.payload,
+        isLoading: false
+      };
+    case TASKS_LOAD_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case TASKS_POST_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case TASKS_POST_SUCCESS:
+      return {
+        tasks:
+          state.tasks.length > 0
+            ? [...state.tasks, action.payload]
+            : [action.payload],
+        isLoading: false
+      };
+    case TASKS_POST_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case TASKS_PUT_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case TASKS_PUT_SUCCESS:
+      // const filteredTasks = state.projects.protasks.map(project => {
+      //   if (project.project.id !== action.payload.project.id) {
+      //     return project;
+      //   } else {
+      return action.payload;
+    //   }
+    // });
+    // return {
+    //   ...state,
+    //   tasks: filtered,
+    //   isLoading: false
+    // };
+    case TASKS_PUT_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+
+    case TASKS_DELETE_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case TASKS_DELETE_SUCCESS:
+      return {
+        ...state,
+        tasks: action.payload
+      };
+    case TASKS_DELETE_FAILURE:
       return {
         ...state,
         error: action.payload,
