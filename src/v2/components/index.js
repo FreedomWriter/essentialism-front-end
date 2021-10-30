6cb812f2bd69fad56a1977b2b55b5f058f2ac41c
FreedomWriter <natalienetworks@gmail.com>
2021-10-30 17:49:12 -0500

    📦 NEW: footer placeholder to verify the page height css is functioning as expected

--- src/v2/components/index.js
+++ src/v2/components/index.js
@@ -3,4 +3,5 @@ export * from "./AuthComponents";
 export * from "./Modals";
 
 export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
 export { default as ModalHandler } from "./ModalHandler";
