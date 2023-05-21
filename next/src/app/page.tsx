45cee27ea1e1e3e6f2108e106f42807961f4c892
FreedomWriter <natalienetworks@gmail.com>
2023-05-20 23:25:57 -0500

    importing react

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Homepage } from "@components";
 import { useTheme } from "@hooks";
 import { GlobalStyles } from "@utils";
+import React from "react";
 
 export default function App() {
   const [theme, toggleTheme] = useTheme();
--- next/src/components/HomePage/Homepage.tsx
+++ next/src/components/HomePage/Homepage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container } from "./styled";
 
 export default function Home() {
