64134e8244f82c5738cee26d88496e7276a3eb54
FreedomWriter <natalienetworks@gmail.com>
2023-05-25 10:09:23 -0500

    📦 NEW: button component ported

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,34 +2,13 @@
 import { Homepage } from "@components";
 import { useTheme } from "@hooks";
 import { GlobalStyles } from "@utils";
-import { Button } from "@ui";
 import React from "react";
 
 export default function App() {
-  const [theme, toggleTheme] = useTheme();
+  const [theme] = useTheme();
 
   return (
     <>
-      <Button onClick={toggleTheme}>Toggle Darkmode</Button>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "16px",
-        }}
-      >
-        <Button onClick={() => console.log("clicked: Default")}>Default</Button>
-        <Button
-          variant="outline"
-          onClick={() => console.log("clicked: Outline")}
-        >
-          Outline
-        </Button>
-        <Button variant="ghost" onClick={() => console.log("clicked: Ghost")}>
-          Ghost
-        </Button>
-      </div>
       <GlobalStyles theme={theme} />
       <Homepage />
     </>
